73c26205a1f70ee771fa2261f20b93da2a9c6369
TDillman <tobydillman@gmail.com>
2021-04-16 13:09:31 -0500

    Testing integration with Github

--- main.py
+++ main.py
@@ -15,8 +15,6 @@ handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w'
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
-#test
-
 nest_asyncio.apply()
 
 client_id = os.environ['CLIENT_ID']
