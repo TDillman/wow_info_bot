a62496d6cd38f1b666bcb95bd6a71c7f7a2351b1
TDillman <tobydillman@gmail.com>
2021-05-14 19:36:29 -0500

    Turns out json.dumps is basically magic.

--- character_armory_collector.py
+++ character_armory_collector.py
@@ -1,8 +1,7 @@
 import os
-import time
+import json
 
 from alive_progress import alive_bar
-
 from blizzardapi import BlizzardApi
 
 client_id = os.environ['CLIENT_ID']
@@ -23,36 +22,35 @@ char_list.sort()
 with alive_bar(len(char_list)) as bar:
     for character_name in char_list:
         try:
-            character_gear_object = api_client.wow.profile.get_character_profile_summary("us", "en_US", server_slug,
-                                                                                         character_name.lower())
-            with open(f"/home/ubuntu/might_bot/gear/{character_name}.json", "w+") as gear_file:
-                gear_file.write(str(character_gear_object).replace("\'", "\""))
+            character_gear_object = json.dumps(api_client.wow.profile.get_character_profile_summary("us", "en_US", server_slug,
+                                                                                                    character_name.lower()))
+            with open(f"/home/ubuntu/might_bot/gear/{character_name}.json", "w") as gear_file:
+                gear_file.write(character_gear_object)
         except:
             os.remove(f"/home/ubuntu/might_bot/gear/{character_name}.json")
 
         try:
-            character_image_object = api_client.wow.profile.get_character_media_summary("us", "en_US", server_slug,
-                                                                                        character_name.lower())
-            with open(f"/home/ubuntu/might_bot/media/{character_name}.json", "w+") as media_file:
-                media_file.write(str(character_image_object).replace("\'", "\""))
+            character_image_object = json.dumps(api_client.wow.profile.get_character_media_summary("us", "en_US", server_slug,
+                                                                                                   character_name.lower()))
+            with open(f"/home/ubuntu/might_bot/media/{character_name}.json", "w") as media_file:
+                media_file.write(character_image_object)
         except:
             os.remove(f"/home/ubuntu/might_bot/media/{character_name}.json")
 
         try:
-            character_equipment_object = api_client.wow.profile.get_character_equipment_summary("us", "en_US", server_slug,
-                                                                                                character_name.lower())
-            with open(f"/home/ubuntu/might_bot/equipment/{character_name}.json", "w+") as equipment_file:
-                equipment_file.write(str(character_equipment_object).replace("\'", "\""))
+            character_equipment_object = json.dumps(api_client.wow.profile.get_character_equipment_summary("us", "en_US", server_slug,
+                                                                                                           character_name.lower()))
+            with open(f"/home/ubuntu/might_bot/equipment/{character_name}.json", "w") as equipment_file:
+                equipment_file.write(character_equipment_object)
         except:
             os.remove(f"/home/ubuntu/might_bot/equipment/{character_name}.json")
 
         try:
-            character_achievement_object = api_client.wow.profile.get_character_achievements_summary("us", "en_US", server_slug,
-                                                                                                     character_name.lower())
-            with open(f"/home/ubuntu/might_bot/achievement/{character_name}.json", "w+") as achievement_file:
-                achievement_file.write(str(character_achievement_object).replace("\'", "\""))
+            character_achievement_object = json.dumps(api_client.wow.profile.get_character_achievements_summary("us", "en_US", server_slug,
+                                                                                                                character_name.lower()))
+            with open(f"/home/ubuntu/might_bot/achievement/{character_name}.json", "w") as achievement_file:
+                achievement_file.write(character_achievement_object)
         except:
             os.remove(f"/home/ubuntu/might_bot/achievement/{character_name}.json")
 
-        time.sleep(.5)
         bar()
