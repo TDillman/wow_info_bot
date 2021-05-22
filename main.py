fec3f2808ad8446540d49ee0be1aa76161dd263f
TDillman <tobydillman@gmail.com>
2021-05-22 13:01:28 -0500

    Modified listeners.

--- main.py
+++ main.py
@@ -39,26 +39,9 @@ shit_list = ['https://i.pinimg.com/originals/ef/a6/48/efa648c67f3cb05287ded99612
              ]
 
 command_dict = {
-        '!scrumpy': 'Thinks your bags are awful',
-        '!calendar': '<@267888830634262529> needs to update the calendar!',
-        '!golfclap': 'https://tenor.com/view/charlie-sheen-emilio-estevez-golf-clap-men-at-work-gif-7577611',
-        '!spooky': 'https://img1.cgtrader.com/items/1911482/ca45cf3f32/air-horn-3d-model-max-obj-mtl-3ds-fbx-dxf.jpg',
-        '!whatever': 'https://media.discordapp.net/attachments/765619338337058827/802299499908563024/whatever.gif',
-        '!cool': 'https://tenor.com/view/andy-samberg-brooklyn99-jake-peralta-cool-gif-12063970',
         'cool cool cool': 'https://tenor.com/view/andy-samberg-brooklyn99-jake-peralta-cool-gif-12063970',
-        '!myst': 'https://tenor.com/view/is-it-though-thor-smile-gif-13334930',
-        '!beylock': 'https://imgur.com/a/xux2u6p',
         'flex': 'https://cdn.discordapp.com/attachments/676183284123828236/823278892676022353/image0.jpg',
-        '!happybirthday': 'https://giphy.com/gifs/i8htPQwChFOVcpnImq',
-        '!magic': 'https://media.discordapp.net/attachments/676183284123828236/761438362720272394/Kat_Confetti.gif',
-        '!lynkz': 'https://tenor.com/view/james-franco-fuck-that-dude-fuckoff-annoyed-annoying-gif-11146686',
         'listen here you little shit': random.choice(shit_list),
-        '!candercane': 'https://giphy.com/gifs/angry-mad-anger-l1J9u3TZfpmeDLkD6',
-        '!wat': 'https://imgur.com/a/PnB5eFk',
-        '!thisisfine': 'https://imgur.com/a/uDAO5In',
-        '!pirate': 'https://imgur.com/a/TDot4Ba',
-        '!justice': 'https://gfycat.com/adorablespotlesshammerheadbird',
-        '!suckit': 'https://imgur.com/Fy6RhWI',
         'suck it': 'https://imgur.com/Fy6RhWI'
     }
 
