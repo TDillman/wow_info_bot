3e997a8c28230d934bfe46ac5dae84752e62140e
TDillman <tobydillman@gmail.com>
2021-05-31 21:50:56 -0500

    Put in a shitty hack to ignore case in month argument

--- cogs/raffle.py
+++ cogs/raffle.py
@@ -22,7 +22,19 @@ month_dict = {
     "September": 8,
     "October": 9,
     "November": 10,
-    "December": 11
+    "December": 11,
+    "january": 0,
+    "february": 1,
+    "march": 2,
+    "april": 3,
+    "may": 4,
+    "june": 5,
+    "july": 6,
+    "august": 7,
+    "september": 8,
+    "october": 9,
+    "november": 10,
+    "december": 11
 }
 
 if not os.path.isfile("config.yaml"):
