6ef1fae3de4da836ae6c0bbe9064e3a3bea55312
TDillman <tobydillman@gmail.com>
2021-06-01 09:51:36 -0500

    Sorted raffle list alphabetically

--- cogs/raffle.py
+++ cogs/raffle.py
@@ -63,10 +63,13 @@ class Raffle(commands.Cog, name="raffle"):
                 title="Monthly Might Raffle",
                 color=config["success"]
             )
-            embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/676183284123828236/679823287521771602/mightcoloredfinishedsmall.png")
-            embed.add_field(name="And the winner is...",
-                            value=f'\n**{raffle_winner}**'
-                            )
+            embed.set_thumbnail(
+                url="https://cdn.discordapp.com/attachments/676183284123828236/679823287521771602/mightcoloredfinishedsmall.png"
+            )
+            embed.add_field(
+                name="And the winner is...",
+                value=f'\n**{raffle_winner}**'
+            )
             await ctx.channel.send(embed=embed)
         else:
             embed = discord.Embed(
@@ -81,7 +84,7 @@ class Raffle(commands.Cog, name="raffle"):
     async def addticket(self, ctx, *, args):
         """
         Award someone a ticket.
-            Usage: !addticket "<User - Reason>"
+            Usage: !addticket <User - Reason>
             example: !addticket HealtoDeath - Raid Attendance
         """
         current_month = datetime.now().month - 1
@@ -95,7 +98,8 @@ class Raffle(commands.Cog, name="raffle"):
                 color=config["success"]
             )
             embed.set_thumbnail(
-                url="https://cdn.discordapp.com/attachments/676183284123828236/679823287521771602/mightcoloredfinishedsmall.png")
+                url="https://cdn.discordapp.com/attachments/676183284123828236/679823287521771602/mightcoloredfinishedsmall.png"
+            )
             await ctx.channel.send(embed=embed)
         else:
             embed = discord.Embed(
@@ -117,30 +121,40 @@ class Raffle(commands.Cog, name="raffle"):
         ticket_string_first = ''
         ticket_string_second = ''
         half_list = int(len(sheet.col_values(1)) / 2)
-        ticket_list_first = sheet.col_values(1)[half_list:]
-        ticket_list_second = sheet.col_values(1)[:half_list]
-        for ticket in ticket_list_first:
-            ticket_string_first += f'{ticket}\n'
-        for ticket in ticket_list_second:
-            ticket_string_second += f'{ticket}\n'
-        embed = discord.Embed(
-            title=f"Raffle Tickets for {arg}!",
-            description=f"{arg} 2021",
-            color=config["success"]
-        )
-        embed.set_thumbnail(
-            url="https://cdn.discordapp.com/attachments/676183284123828236/679823287521771602/mightcoloredfinishedsmall.png")
-        embed.add_field(
-            name="1/2",
-            value=ticket_string_first,
-            inline=False
-        )
-        embed.add_field(
-            name="2/2",
-            value=ticket_string_second,
-            inline=False
-        )
-        await ctx.channel.send(embed=embed)
+        total_ticket_list = sheet.col_values(1)
+        total_ticket_list.sort()
+        ticket_list_first = total_ticket_list[half_list:]
+        ticket_list_second = total_ticket_list[:half_list]
+        if len(sheet.col_values(1)) == 0:
+            embed = discord.Embed(
+                title='No raffle tickets found for this month',
+                description='Try another month that has raffle tickets recorded.'
+            )
+            await ctx.channel.send(embed=embed)
+        else:
+            for ticket in ticket_list_first:
+                ticket_string_first += f'{ticket}\n'
+            for ticket in ticket_list_second:
+                ticket_string_second += f'{ticket}\n'
+            embed = discord.Embed(
+                title=f"Raffle Tickets for {arg}!",
+                description=f"{arg} 2021",
+                color=config["success"]
+            )
+            embed.set_thumbnail(
+                url="https://cdn.discordapp.com/attachments/676183284123828236/679823287521771602/mightcoloredfinishedsmall.png"
+            )
+            embed.add_field(
+                name="\u200b",
+                value=ticket_string_second,
+                inline=True
+            )
+            embed.add_field(
+                name="\u200b",
+                value=ticket_string_first,
+                inline=True
+            )
+            await ctx.channel.send(embed=embed)
 
 
 def setup(bot):
