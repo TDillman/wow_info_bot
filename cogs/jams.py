3faa0205302182fdf2c355522279f793dd7a733c
TDillman <tobydillman@gmail.com>
2021-06-10 16:48:06 -0500

    Removed the "you may also like" embed since no one liked it.

--- cogs/jams.py
+++ cogs/jams.py
@@ -1,7 +1,6 @@
 import os
 import sys
 import yaml
-import discord
 
 from youtube_api import YouTubeDataAPI
 from discord.ext import commands
@@ -32,21 +31,6 @@ class Jams(commands.Cog, name="jams"):
         else:
             youtube_video_url = f"https://www.youtube.com/watch?v={results[0]['video_id']}"
             await ctx.channel.send(youtube_video_url)
-            embed = discord.Embed(
-                title=f"Not what you're looking for?",
-                color=config["success"]
-            )
-            if len(results) < 5:
-                for video in results[1:len(results)]:
-                    video_string += f"[{video['video_title']}](https://www.youtube.com/watch?v={video['video_id']})\n"
-            if len(results) >= 5:
-                for video in results[1:6]:
-                    video_string += f"[{video['video_title']}](https://www.youtube.com/watch?v={video['video_id']})\n"
-            embed.add_field(
-                name=f"More videos for {args}:",
-                value=video_string
-            )
-            await ctx.channel.send(embed=embed)
 
 
 def setup(bot):
