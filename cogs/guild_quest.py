2413d9e591d2b27502e530486cd3edcc794e8fde
TDillman <tobydillman@gmail.com>
2021-06-21 16:26:03 -0500

    Updated guild quest. Need to start thinking about how to enable this for the next officer. If they want to, even. I'll cross that bridge later.

--- cogs/guild_quest.py
+++ cogs/guild_quest.py
@@ -29,45 +29,34 @@ class GuildQuest(commands.Cog, name="guildquest"):
                 color=config["success"]
             )
             embed1.add_field(
-                name="Guild Quest: June 15th, 2021",
-                value=f"{guild_member_role.mention} -- Guild Quest Tuesday! If you do things right, you'll get "
-                      f"two toys from this one. But there's a catch: You have to be a Horde character to complete "
-                      f"this particular quest. Our main quest is to find the [Zandalari Effigy Amulet]"
-                      f"(https://ptr.wowhead.com/item=156649/zandalari-effigy-amulet)"
+                name="Guild Quest: June 22nd, 2021",
+                value=f"{guild_member_role.mention} -- Guild Quest Tuesday! Technically this quest isn't today. It's "
+                      f"next Sunday, the 27th. This week's guild quest is the Running of the Trolls!"
             )
             embed2 = discord.Embed(
-                description="Location",
+                description="Running of the Trolls",
                 color=config["success"]
             )
             embed2.add_field(
-                name="Where to Start",
-                value="Start in Dazar'Alor's Grand Bazaar, with Griftah (our BC Classic brethren may recognize him "
-                      "from Shattrath City).\n\nhttps://i.imgur.com/pWQy8RT.jpg")
-            embed2.set_image(url="https://wow.zamimg.com/uploads/screenshots/normal/742830-griftah.jpg")
+                name="What It Is",
+                value="A bunch of folks create Troll characters on Feathermoon-US and run from the starting zone "
+                      "all the way up to Silvermoon City. The event is to raise awareness for [The Trevor Project](https://give.thetrevorproject.org/TrollRun2021), "
+                      "which is a foundation aimed at ending LGBTQ youth suicide.")
+            embed2.set_image(url="https://i.redd.it/0owzblq9vb571.jpg")
             embed3 = discord.Embed(
-                description="Map of Dazar'Alor",
-                colour=config["success"]
-            )
-            embed3.set_image(url="https://i.imgur.com/pWQy8RT.jpg")
-            embed4 = discord.Embed(
-                description="Time to Trade",
+                description="When and Where",
                 color=config["success"]
             )
-            embed4.add_field(
-                name="Steps:",
-                value=f"1. Purchase a  Sack of 'Discarded' Hearthstones from Griftah (52.9, 89.9).\n"
-                      f"2. Go to Rakle the Wretched (34.8, 11.5) and trade the  Sack of 'Discarded' Hearthstones for a  Much-Too-Hot Pepper.\n"
-                      f"3. Go to Trader Haw'li (37.8, 14.7) and trade the  Much-Too-Hot Pepper for some  Golden Seeds. **This "
-                      f"vendor also sells Haw'li's Hot & Spicy Chili, so you can pick that up here too**\n"
-                      f"4. Go to Granda Watae (42.2, 35.7) and trade the  Golden Seeds for a  Centennial Blossom.\n"
-                      f"5. Go to Trader Nog (57.2, 91.5) and trade the  Centennial Blossom for a  Preserved Night Elf Head.\n"
-                      f"6. Go to 'Black Eye' Zenru (53.8, 86) and trade the  Preserved Night Elf Head for a  Counterfeit Rastakhan Mask.\n"
-                      f"7. Finally, return to Griftah (52.9, 89.9) and trade the  Counterfeit Rastakhan Mask for the  Zandalari Effigy Amulet.\n"
+            embed3.add_field(
+                name="Feathermoon-US on Sunday, June 27th\n6pm Pacific Time/9pm Eastern Time",
+                value="Whoever would like to join, come hang out and run with us. I'll be creating a character there "
+                      "and logging in around 5:30PM Pacific Time just to get ready and hang out for a bit, then participating "
+                      "in the run when it's time. Everyone who joins us for the run gets a raffle ticket!\n\nAlso I'll "
+                      "donate $5 to the Trevor Project for every member of Might who runs with us."
             )
             await context.send(embed=embed1)
             await context.send(embed=embed2)
             await context.send(embed=embed3)
-            await context.send(embed=embed4)
             await context.message.delete()
 
 
