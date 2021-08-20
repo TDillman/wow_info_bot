b6f362aecbb12bf299603949fcee7c54374401cc
TDillman <tobydillman@gmail.com>
2021-08-19 23:26:51 -0500

    Found a way to make Kat hate me forever

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ https://github.com/kkrypt0nn/Python-Discord-Bot-Template/blob/master/bot.py
 
 import discord
 import nest_asyncio
+import asyncio
 import os
 import platform
 import yaml
@@ -155,21 +156,55 @@ async def on_command_error(context, error):
     raise error
 
 
-@tasks.loop(hours=24)
-async def affirmation():
-    url = 'https://www.affirmations.dev'
+# @tasks.loop(hours=24)
+# async def affirmation():
+#     url = 'https://www.affirmations.dev'
+#
+#     request = requests.get(url)
+#     affirmation_object = json.loads(request.text)
+#
+#     # Create embed in Discord
+#     embed = discord.Embed(title="Daily Affirmation", color=config["success"])
+#     embed.set_thumbnail(url='https://www.pitara.com/media/lotus-flower_hu4aeaffed200a0cfd996c55c6e5156325_90396_600x0_resize_q60_box.jpg')
+#     embed.add_field(name='Affirmation', value=affirmation_object['affirmation'])
+#
+#     # Declare #might-health as destination channel
+#     await bot.get_channel(710566582845177888).send(embed=embed)
 
-    request = requests.get(url)
-    affirmation_object = json.loads(request.text)
 
-    # Create embed in Discord
-    embed = discord.Embed(title="Daily Affirmation", color=config["success"])
-    embed.set_thumbnail(url='https://www.pitara.com/media/lotus-flower_hu4aeaffed200a0cfd996c55c6e5156325_90396_600x0_resize_q60_box.jpg')
-    embed.add_field(name='Affirmation', value=affirmation_object['affirmation'])
+@bot.command(name='broncena', help='Make Kat hate you forever')
+async def join(ctx):
+    if ctx.message.author.id in config["owners"]:
+        if not ctx.message.author.voice:
+            await ctx.send("{} is not connected to a voice channel".format(ctx.message.author.name))
+            return
+        else:
+            channel = ctx.message.author.voice.channel
+        await channel.connect()
+        try:
+            server = ctx.message.guild
+            voice_channel = server.voice_client
+            async with ctx.typing():
+                filename = "broncena-intro.ogg"
+                voice_channel.play(discord.FFmpegPCMAudio(executable="/usr/bin/ffmpeg", source=filename))
+            await ctx.send(f'**Now playing:** {filename}')
+        except:
+            await ctx.send("The bot is not connected to a voice channel.")
+        voice_client = ctx.message.guild.voice_client
+        if voice_client.is_connected():
+            await asyncio.sleep(13)
+            await voice_client.disconnect()
+        else:
+            await ctx.send("The bot is not connected to a voice channel.")
 
-    # Declare #might-health as destination channel
-    await bot.get_channel(710566582845177888).send(embed=embed)
 
+@bot.command(name='stop', help='Stops the BRON')
+async def stop(ctx):
+    voice_client = ctx.message.guild.voice_client
+    if voice_client.is_playing():
+        await voice_client.stop()
+    else:
+        await ctx.send("The bot is not playing anything at the moment.")
 
 # Run the bot with the token
 bot.run(config["discord_token"])
